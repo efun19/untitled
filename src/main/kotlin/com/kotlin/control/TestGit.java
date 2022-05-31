07c30d5fbd771479908275469d80affd4dbf8b8b
efun19 <1027431799@qq.com>
2022-05-31 18:37:37 +0800

    third

--- src/main/kotlin/com/kotlin/control/TestGit.java
+++ src/main/kotlin/com/kotlin/control/TestGit.java
@@ -3,5 +3,7 @@ package com.kotlin.control;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
     }
 }
